14d8e5080fa78a0e28d669122821573f273825d8
pjn141414 <jina071049@gmail.com>
2021-07-13 14:50:12 +0900

    Add: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CommunityModule } from './community/community.module';
+import { DuckMovieModule } from './duck-movie/duck-movie.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), AuthModule, CommunityModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, CommunityModule, DuckMovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
